3e663b2eec5b0579b84d694c0bf1f332eb072242
Akashjaura016@gmail.com <akashjaura016@gmail.com>
2025-09-24 13:15:28 +1000

    Trigger CI on testing branch

--- backend/order_service/app/main.py
+++ backend/order_service/app/main.py
@@ -14,7 +14,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import Session, joinedload
 
-from .db import Base, SessionLocal, engine, get_db
+from app.db import Base, SessionLocal, engine, get_db
+
 from .models import Order, OrderItem
 from .schemas import (
     OrderCreate,
--- backend/order_service/app/models.py
+++ backend/order_service/app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, DateTime, ForeignKey, Integer, Numeric, String, T
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
-from .db import Base
+from .app.db import Base
 
 
 class Order(Base):
